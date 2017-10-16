41a72b5ab8977e1b3d6fc880b9e5343fd95ce738
afly6899 <aflai@uci.edu>
2017-10-16 01:10:42 -0400

    Trying to see if I can commit a Unity project
    In Edit -> Project Settings -> Editor:
        1) set Version Control to Visible Meta Files
        2) set Asset Serialization to Force Text
    
    In .gitignore:
        1) changed all the directories to not start with root (/)

---
+++ TestUnity/Assets/onClickChangeBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onClickChangeBackground : MonoBehaviour {
+	public Sprite sprite1;
+	public Sprite sprite2;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		if (spriteRenderer == null) {
+			spriteRenderer.sprite = sprite1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				changeSprite ();
+			}
+				
+	}
+
+			
+	void changeSprite() {
+		if (spriteRenderer.sprite == sprite1) {
+			spriteRenderer.sprite = sprite2;
+		} else {
+			spriteRenderer.sprite = sprite1;
+		}
+	}
+}
